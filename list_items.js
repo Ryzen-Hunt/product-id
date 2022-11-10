8cc945d608ffe9a72981802c1c615baa68199dbf
Ryzen_Hunt <57281810+Ryzen-Hunt@users.noreply.github.com>
2022-11-10 20:56:14 +0530

    Create list_items.js

---
+++ list_items.js
@@ -0,0 +1,5 @@
+<script>
+  function showAlert() {
+    alert ("This website is developed by former employee (Anikesh) of Full DIgital Ads, who was not paid for 2 months and he was fired without any notice and salary on 10-11-2022 thursday ");
+  }
+</script>
